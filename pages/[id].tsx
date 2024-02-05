a89277624b355bd896cf2fa8f1655394a6d7e5b2
Facundo Fazzito <facufazzito@gmail.com>
2024-02-05 08:50:16 -0300

    fixed error

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -8,23 +8,24 @@ const ProductDetails = () => {
   const router = useRouter();
   const { id } = router.query;
 
-  const [data , setData] = useState( [{id: "0" }]);
+  const [data , setData] = useState( [{id: "1" }]);
   const [isLoading, setLoading] = useState(true);
   const [product, setProduct] = useState( [{id: "0" }]);
 
-  useEffect(() => {
-    fetch("/api/data.json")
-    .then((res) => res.json())
-    .then((response) => {
-      setData(response);
-      setLoading(false);
-      response.map((item:any) => {
-        if (item.id.toString() === id) {
-          setProduct(item)
-        }
-        })
-      })
-    }, [id]);
+    useEffect(() => {
+      
+    fetch("/api/data")
+      .then((res) => res.json())
+      .then((response) => {
+        setLoading(false);
+        response.map((item: any) => {
+          if (item.id.toString() === id) {
+            setProduct(item);
+          }
+        });
+      });
+  }, [id]);
+
 
     if(isLoading){
       return <p>Loading...</p>
